4b4f47f5a5e7afa42c4a9f05500818e5e893039b
saharoori <saharlaor@gmail.com>
2022-02-03 22:56:49 +0200

    Fix prod client API URL typo

--- client/src/api/eventApi.js
+++ client/src/api/eventApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const url =
   process.env.NODE_ENV === "production"
-    ? "/api/event"
+    ? "/api/events"
     : `http://localhost:${process.env.port || 8000}/api/events`;
 
 export default axios.create({ baseURL: url });
